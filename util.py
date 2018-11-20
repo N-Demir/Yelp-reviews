392e9029b60c2d1371fb05e0a89d8c61abce87dd
Nikita Demir <nikitde1@gmail.com>
2018-11-19 16:22:22 -0800

    Added KFold cross validation

--- util.py
+++ util.py
@@ -41,6 +41,26 @@ def load_review_dataset(folder, folds):
 			fake = False
 
 	return reviews, np.array(labels)
+
+def load_review_dataset_full(folder):
+	reviews = []
+	labels = []
+	fake = False
+	for root, dirs, files in os.walk(folder):
+		directory = os.path.basename(root)
+		if ("deceptive" in directory):
+			fake = True
+		elif ("truthful" in directory):
+			fake = False
+
+		for file in files:
+			with open(os.path.join(root, file)) as f:
+				for review in f.readlines():
+					reviews.append(review.strip())
+					labels.append(1 if fake else 0)
+
+	return np.array(reviews), np.array(labels)
+
 		
 
 def write_json(filename, value):
